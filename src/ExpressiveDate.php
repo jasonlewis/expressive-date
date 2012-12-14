ff8264c3aa383ca2142317ff2c9378874323b765
Jason Lewis <jason.lewis1991@gmail.com>
2012-12-14 19:36:20 +1100

    Initial commit of ExpressiveDate
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ src/ExpressiveDate.php
@@ -0,0 +1,906 @@
+<?php
+
+class ExpressiveDate extends DateTime {
+
+	/**
+	 * Create a new date instance.
+	 * 
+	 * @param  string  $time
+	 * @param  string|DateTimeZone  $timezone
+	 * @return void
+	 */
+	public function __construct($time = null, $timezone = null)
+	{
+		$timezone = $this->parseSuppliedTimezone($timezone);
+
+		parent::__construct($time, $timezone);
+	}
+
+	/**
+	 * Parse a supplied timezone.
+	 * 
+	 * @param  string|DateTimeZone  $timezone
+	 * @return DateTimeZone
+	 */
+	protected function parseSuppliedTimezone($timezone)
+	{
+		if ($timezone instanceof DateTimeZone or is_null($timezone))
+		{
+			return $timezone;
+		}
+
+		try
+		{
+			$timezone = new DateTimeZone($timezone);
+		}
+		catch (Exception $error)
+		{
+			throw new InvalidArgumentException('The supplied timezone ['.$timezone.'] is not supported.');
+		}
+
+		return $timezone;
+	}
+
+	/**
+	 * Use the current date and time.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function now()
+	{
+		$this->setTimestamp(time());
+
+		return $this;
+	}
+
+	/**
+	 * Use today's date and time at midnight.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function today()
+	{
+		$this->now()->setHour(0)->setMinute(0)->setSecond(0);
+
+		return $this;
+	}
+
+	/**
+	 * Use tomorrow's date and time at midnight.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function tomorrow()
+	{
+		$this->now()->addOneDay()->startOfDay();
+
+		return $this;
+	}
+
+	/**
+	 * Use yesterday's date and time at midnight.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function yesterday()
+	{
+		$this->now()->minusOneDay()->startOfDay();
+
+		return $this;
+	}
+
+	/**
+	 * Use the start of the day.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function startOfDay()
+	{
+		$this->setHour(0)->setMinute(0)->setSecond(0);
+
+		return $this;
+	}
+
+	/**
+	 * Use the end of the day.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function endOfDay()
+	{
+		$this->setHour(23)->setMinute(59)->setSecond(59);
+
+		return $this;
+	}
+
+	/**
+	 * Use the start of the month.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function startOfMonth()
+	{
+		$this->setDay(1)->startOfDay();
+
+		return $this;
+	}
+
+	/**
+	 * Use the end of the month.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function endOfMonth()
+	{
+		$this->setDay($this->getDaysInMonth())->endOfDay();
+
+		return $this;
+	}
+
+	/**
+	 * Add one day.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addOneDay()
+	{
+		return $this->modifyDays(1);
+	}
+
+	/**
+	 * Add a given amount of days.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addDays($amount)
+	{
+		return $this->modifyDays($amount);
+	}
+
+	/**
+	 * Minus one day.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function minusOneDay()
+	{
+		return $this->modifyDays(1, true);
+	}
+
+	/**
+	 * Minus a given amount of days.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function minusDays($amount)
+	{
+		return $this->modifyDays($amount, true);
+	}
+
+	/**
+	 * Modify by an amount of days.
+	 * 
+	 * @param  int  $amount
+	 * @param  bool  $invert
+	 * @return ExpressiveDate
+	 */
+	protected function modifyDays($amount, $invert = false)
+	{
+		$interval = new DateInterval("P{$amount}D");
+
+		$this->modifyFromInterval($interval, $invert);
+
+		return $this;
+	}
+
+	/**
+	 * Add one month.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addOneMonth()
+	{
+		return $this->modifyMonths(1);
+	}
+
+	/**
+	 * Add a given amount of months.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addMonths($amount)
+	{
+		return $this->modifyMonths($amount);
+	}
+
+	/**
+	 * Minus one month.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function minusOneMonth()
+	{
+		return $this->modifyMonths(1, true);
+	}
+
+	/**
+	 * Minus a given amount of months.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function minusMonths($amount)
+	{
+		return $this->modifyMonths($amount, true);
+	}
+
+	/**
+	 * Modify by an amount of months.
+	 * 
+	 * @param  int  $amount
+	 * @param  bool  $invert
+	 * @return ExpressiveDate
+	 */
+	protected function modifyMonths($amount, $invert = false)
+	{
+		$interval = new DateInterval("P{$amount}M");
+
+		$this->modifyFromInterval($interval, $invert);
+
+		return $this;
+	}
+
+	/**
+	 * Add one year.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addOneYear()
+	{
+		return $this->modifyYears(1);
+	}
+
+	/**
+	 * Add a given amount of years.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addYears($amount)
+	{
+		return $this->modifyYears($amount);
+	}
+
+	/**
+	 * Minus one year.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function minusOneYear()
+	{
+		return $this->modifyYears(1, true);
+	}
+
+	/**
+	 * Minus a given amount of years.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function minusYears($amount)
+	{
+		return $this->modifyYears($amount, true);
+	}
+
+	/**
+	 * Modify by an amount of Years.
+	 * 
+	 * @param  int  $amount
+	 * @param  bool  $invert
+	 * @return ExpressiveDate
+	 */
+	protected function modifyYears($amount, $invert = false)
+	{
+		$interval = new DateInterval("P{$amount}Y");
+
+		$this->modifyFromInterval($interval, $invert);
+
+		return $this;
+	}
+
+	/**
+	 * Add one hour.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addOneHour()
+	{
+		return $this->modifyHours(1);
+	}
+
+	/**
+	 * Add a given amount of hours.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addHours($amount)
+	{
+		return $this->modifyHours($amount);
+	}
+
+	/**
+	 * Minus one hour.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function minusOneHour()
+	{
+		return $this->modifyHours(1, true);
+	}
+
+	/**
+	 * Minus a given amount of hours.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function minusHours($amount)
+	{
+		return $this->modifyHours($amount, true);
+	}
+
+	/**
+	 * Modify by an amount of hours.
+	 * 
+	 * @param  int  $amount
+	 * @param  bool  $invert
+	 * @return ExpressiveDate
+	 */
+	protected function modifyHours($amount, $invert = false)
+	{
+		$interval = new DateInterval("PT{$amount}H");
+
+		$this->modifyFromInterval($interval, $invert);
+
+		return $this;
+	}
+
+	/**
+	 * Add one minute.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addOneMinute()
+	{
+		return $this->modifyMinutes(1);
+	}
+
+	/**
+	 * Add a given amount of minutes.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addMinutes($amount)
+	{
+		return $this->modifyMinutes($amount);
+	}
+
+	/**
+	 * Minus one minute.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function minusOneMinute()
+	{
+		return $this->modifyMinutes(1, true);
+	}
+
+	/**
+	 * Minus a given amount of minutes.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function minusMinutes($amount)
+	{
+		return $this->modifyMinutes($amount, true);
+	}
+
+	/**
+	 * Modify by an amount of minutes.
+	 * 
+	 * @param  int  $amount
+	 * @param  bool  $invert
+	 * @return ExpressiveDate
+	 */
+	protected function modifyMinutes($amount, $invert = false)
+	{
+		$interval = new DateInterval("PT{$amount}M");
+
+		$this->modifyFromInterval($interval, $invert);
+
+		return $this;
+	}
+
+	/**
+	 * Add one second.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addOneSecond()
+	{
+		return $this->modifySeconds(1);
+	}
+
+	/**
+	 * Add a given amount of seconds.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addSeconds($amount)
+	{
+		return $this->modifySeconds($amount);
+	}
+
+	/**
+	 * Minus one second.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function minusOneSecond()
+	{
+		return $this->modifySeconds(1, true);
+	}
+
+	/**
+	 * Minus a given amount of seconds.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function minusSeconds($amount)
+	{
+		return $this->modifySeconds($amount, true);
+	}
+
+	/**
+	 * Modify by an amount of seconds.
+	 * 
+	 * @param  int  $amount
+	 * @param  bool  $invert
+	 * @return ExpressiveDate
+	 */
+	protected function modifySeconds($amount, $invert = false)
+	{
+		$interval = new DateInterval("PT{$amount}S");
+
+		$this->modifyFromInterval($interval, $invert);
+
+		return $this;
+	}
+
+	/**
+	 * Add one week.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addOneWeek()
+	{
+		return $this->modifyWeeks(1);
+	}
+
+	/**
+	 * Add a given amount of weeks.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function addWeeks($amount)
+	{
+		return $this->modifyWeeks($amount);
+	}
+
+	/**
+	 * Minus one week.
+	 * 
+	 * @return ExpressiveDate
+	 */
+	public function minusOneWeek()
+	{
+		return $this->modifyWeeks(1, true);
+	}
+
+	/**
+	 * Minus a given amount of weeks.
+	 * 
+	 * @param  int  $amount
+	 * @return ExpressiveDate
+	 */
+	public function minusWeeks($amount)
+	{
+		return $this->modifyWeeks($amount, true);
+	}
+
+	/**
+	 * Modify by an amount of weeks.
+	 * 
+	 * @param  int  $amount
+	 * @param  bool  $invert
+	 * @return ExpressiveDate
+	 */
+	protected function modifyWeeks($amount, $invert = false)
+	{
+		$interval = new DateInterval("P{$amount}W");
+
+		$this->modifyFromInterval($interval, $invert);
+
+		return $this;
+	}
+
+	/**
+	 * Modify from a DateInterval object.
+	 * 
+	 * @param  DateInterval  $interval
+	 * @param  bool  $invert
+	 * @return ExpressiveDate
+	 */
+	protected function modifyFromInterval($interval, $invert = false)
+	{
+		if ($invert)
+		{
+			$this->sub($interval);
+		}
+		else
+		{
+			$this->add($interval);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Set the timezone.
+	 * 
+	 * @param  string|DateTimeZone  $timezone
+	 * @return ExpressiveDate
+	 */
+	public function setTimezone($timezone)
+	{
+		$timezone = $this->parseSuppliedTimezone($timezone);
+
+		parent::setTimezone($timezone);
+
+		return $this;
+	}
+
+	/**
+	 * Sets the timestamp from a human readable string.
+	 * 
+	 * @param  string  $string
+	 * @return ExpressiveDate
+	 */
+	public function setTimestampFromString($string)
+	{
+		$this->setTimestamp(strtotime($string));
+
+		return $this;
+	}
+
+	/**
+	 * Determine if day is a weekday.
+	 * 
+	 * @return bool
+	 */
+	public function isWeekday()
+	{
+		$day = $this->getDayOfWeek();
+
+		return ! in_array($day, array('Saturday', 'Sunday'));
+	}
+
+	/**
+	 * Determine if day is a weekend.
+	 * 
+	 * @return bool
+	 */
+	public function isWeekend()
+	{
+		return ! $this->isWeekday();
+	}
+
+	/**
+	 * Get the difference in years.
+	 * 
+	 * @param  ExpressiveDate  $compare
+	 * @return string
+	 */
+	public function getDifferenceInYears($compare = null)
+	{
+		if ( ! $compare)
+		{
+			$compare = new Date(null, $this->getTimezone());
+		}
+
+		return $this->diff($compare)->format('%r%y');
+	}
+
+	/**
+	 * Get the difference in months.
+	 * 
+	 * @param  ExpressiveDate  $compare
+	 * @return string
+	 */
+	public function getDifferenceInMonths($compare = null)
+	{
+		if ( ! $compare)
+		{
+			$compare = new Date(null, $this->getTimezone());
+		}
+
+		return $this->diff($compare)->format('%r%m');
+	}
+
+	/**
+	 * Get the difference in days.
+	 * 
+	 * @param  ExpressiveDate  $compare
+	 * @return string
+	 */
+	public function getDifferenceInDays($compare = null)
+	{
+		if ( ! $compare)
+		{
+			$compare = new Date(null, $this->getTimezone());
+		}
+
+		return $this->diff($compare)->format('%r%a');
+	}
+
+	/**
+	 * Get the difference in hours.
+	 * 
+	 * @param  ExpressiveDate  $compare
+	 * @return string
+	 */
+	public function getDifferenceInHours($compare = null)
+	{
+		return $this->getDifferenceInMinutes($compare) / 60;
+	}
+
+	/**
+	 * Get the difference in minutes.
+	 * 
+	 * @param  ExpressiveDate  $compare
+	 * @return string
+	 */
+	public function getDifferenceInMinutes($compare = null)
+	{
+		return $this->getDifferenceInSeconds($compare) / 60;
+	}
+
+	/**
+	 * Get the difference in seconds.
+	 * 
+	 * @param  ExpressiveDate  $compare
+	 * @return string
+	 */
+	public function getDifferenceInSeconds($compare = null)
+	{
+		if ( ! $compare)
+		{
+			$compare = new Date(null, $this->getTimezone());
+		}
+
+		$difference = $this->diff($compare);
+
+		list($days, $hours, $minutes, $seconds) = explode(':', $difference->format('%a:%h:%i:%s'));
+
+		// Add the total amount of seconds in all the days.
+		$seconds += ($days * 24 * 60 * 60);
+
+		// Add the total amount of seconds in all the hours.
+		$seconds += ($hours * 60 * 60);
+
+		// Add the total amount of seconds in all the minutes.
+		$seconds += ($minutes * 60);
+
+		return $seconds * $difference->format('%r1');
+	}
+
+	/**
+	 * Get a relative date string, e.g., 3 days ago.
+	 * 
+	 * @param  ExpressiveDate  $compare
+	 * @return string
+	 */
+	public function getRelativeDate($compare = null)
+	{
+		if ( ! $compare)
+		{
+			$compare = new ExpressiveDate(null, $this->getTimezone());
+		}
+
+		$units = array('second', 'minute', 'hour', 'day', 'week', 'month', 'year');
+		$values = array(60, 60, 24, 7, 4.35, 12);
+
+		// Get the difference between the two timestamps. We'll use this to cacluate the
+		// actual time remaining.
+		$difference = abs($compare->getTimestamp() - $this->getTimestamp());
+
+		for ($i = 0; $i < count($values) and $difference >= $values[$i]; $i++)
+		{
+			$difference = $difference / $values[$i];
+		}
+
+		// Round the difference to the nearest whole number.
+		$difference = round($difference);
+
+		if ($compare->getTimestamp() < $this->getTimestamp())
+		{
+			$suffix = 'from now';
+		}
+		else
+		{
+			$suffix = 'ago';
+		}
+
+		// Get the unit of time we are measuring. We'll then check the difference, if it is not equal
+		// to exactly 1 then it's a multiple of the given unit so we'll append an 's'.
+		$unit = $units[$i];
+
+		if ($difference != 1)
+		{
+			$unit .= 's';
+		}
+
+		return $difference.' '.$unit.' '.$suffix;
+	}
+
+	/**
+	 * Get a date attribute.
+	 *
+	 * @param  string  $attribute
+	 * @return mixed
+	 */
+	protected function getDateAttribute($attribute)
+	{
+		switch ($attribute)
+		{
+			case 'Day':
+				return $this->format('d');
+				break;
+			case 'Month':
+				return $this->format('m');
+				break;
+			case 'Year':
+				return $this->format('Y');
+				break;
+			case 'Hour':
+				return $this->format('G');
+				break;
+			case 'Minute':
+				return $this->format('i');
+				break;
+			case 'Second':
+				return $this->format('s');
+				break;
+			case 'DayOfWeek':
+				return $this->format('l');
+				break;
+			case 'DayOfWeekAsNumeric':
+				return $this->format('w');
+				break;
+			case 'DaysInMonth':
+				return $this->format('t');
+				break;
+			case 'DayOfYear':
+				return $this->format('z');
+				break;
+			case 'DaySuffix':
+				return $this->format('S');
+				break;
+			case 'LeapYear':
+				return $this->format('L');
+				break;
+			case 'AmOrPm':
+				return $this->format('a');
+				break;
+			case 'TimezoneIdenfifier':
+				return $this->format('e');
+				break;
+			case 'DaylightSavings':
+				return $this->format('I');
+				break;
+			case 'GmtDifference':
+				return $this->format('O');
+				break;
+			case 'SecondsSinceEpoch':
+				return $this->format('U');
+				break;
+			case 'TimezoneName':
+				return $this->getTimezone()->getName();
+				break;
+		}
+
+		throw new InvalidArgumentException('The date attribute ['.$attribute.'] could not be found.');
+	}
+
+	/**
+	 * Set a date attribute.
+	 *
+	 * @param  string  $attribute
+	 * @return mixed
+	 */
+	protected function setDateAttribute($attribute, $value)
+	{
+		switch ($attribute)
+		{
+			case 'Day':
+				return $this->setDate($this->getYear(), $this->getMonth(), $value);
+				break;
+			case 'Month':
+				return $this->setDate($this->getYear(), $value, $this->getDay());
+				break;
+			case 'Year':
+				return $this->setDate($value, $this->getMonth(), $this->getDay());
+				break;
+			case 'Hour':
+				return $this->setTime($value, $this->getMinute(), $this->getSecond());
+				break;
+			case 'Minute':
+				return $this->setTime($this->getHour(), $value, $this->getSecond());
+				break;
+			case 'Second':
+				return $this->setTime($this->getHour(), $this->getMinute(), $value);
+				break;
+		}
+
+		throw new InvalidArgumentException('The date attribute ['.$attribute.'] could not be set.');
+	}
+
+	/**
+	 * Dynamically handle calls for date attributes and testers.
+	 * 
+	 * @param  string  $method
+	 * @param  array  $parameters
+	 * @return mixed
+	 */
+	public function __call($method, $parameters)
+	{
+		if (substr($method, 0, 3) == 'get' or substr($method, 0, 3) == 'set')
+		{
+			$attribute = substr($method, 3);
+		}
+		elseif (substr($method, 0, 2) == 'is')
+		{
+			$attribute = substr($method, 2);
+		}
+
+		if ( ! isset($attribute))
+		{
+			throw new InvalidArgumentException('Could not dynamically handle method call ['.$method.']');
+		}
+
+		if (substr($method, 0, 3) == 'set')
+		{
+			return $this->setDateAttribute($attribute, $parameters[0]);
+		}
+
+		// If not setting an attribute then we'll default to getting an attribute.
+		return $this->getDateAttribute($attribute);
+	}
+
+}
---
+++ tests/ExpressiveDateTest.php
@@ -0,0 +1,366 @@
+<?php
+
+use Mockery as m;
+
+class DateTest extends PHPUnit_Framework_TestCase {
+
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+
+	public function setUp()
+	{
+		date_default_timezone_set('Australia/Melbourne');
+	}
+
+
+	public function testDateIsCreatedFromNow()
+	{
+		$date = new ExpressiveDate;
+		$this->assertEquals(time(), $date->getTimestamp());
+	}
+
+
+	public function testDateIsCreatedWithDifferentTimezone()
+	{
+		$date = new ExpressiveDate(null, 'Europe/Paris');
+		date_default_timezone_set('Europe/Paris');
+		$this->assertEquals(time(), $date->getTimestamp());
+		date_default_timezone_set('Australia/Melbourne');
+		$date = new ExpressiveDate(null, new DateTimeZone('Europe/Paris'));
+		date_default_timezone_set('Europe/Paris');
+		$this->assertEquals(time(), $date->getTimestamp());
+	}
+
+
+	public function testDateIsCreatedWithCustomTimeString()
+	{
+		$date = new ExpressiveDate('31 January 1991');
+		$this->assertEquals('31/01/1991', $date->format('d/m/Y'));
+		$date = new ExpressiveDate('+1 day');
+		$this->assertEquals(time() + 86400, $date->getTimestamp());
+		$date = new ExpressiveDate('-1 day');
+		$this->assertEquals(time() - 86400, $date->getTimestamp());
+	}
+
+
+	public function testCannotCreateDateWithInvalidTimezone()
+	{
+		$this->setExpectedException('InvalidArgumentException');
+		$date = new ExpressiveDate(null, 'Australia/JasonsPlace');
+	}
+
+
+	public function testUseTomorrowsDateAndTime()
+	{
+		$date = new ExpressiveDate;
+		$date->tomorrow();
+		$this->assertEquals(strtotime('tomorrow'), $date->getTimestamp());
+	}
+
+
+	public function testUseYesterdaysDateAndTime()
+	{
+		$date = new ExpressiveDate;
+		$date->yesterday();
+		$this->assertEquals(strtotime('yesterday'), $date->getTimestamp());
+	}
+
+
+	public function testStartOfDay()
+	{
+		$date = new ExpressiveDate;
+		$date->startOfDay();
+		$this->assertEquals(strtotime('today'), $date->getTimestamp());
+	}
+
+
+	public function testEndOfDay()
+	{
+		$date = new ExpressiveDate;
+		$date->endOfDay();
+		$this->assertEquals(strtotime('tomorrow -1 second'), $date->getTimestamp());
+	}
+
+
+	public function testStartOfMonth()
+	{
+		$date = new ExpressiveDate('January');
+		$date->startOfMonth();
+		$this->assertEquals(strtotime('January 1'), $date->getTimestamp());
+	}
+
+
+	public function testEndOfMonth()
+	{
+		$date = new ExpressiveDate('January');
+		$date->endOfMonth();
+		$this->assertEquals(strtotime('February 1 -1 second'), $date->getTimestamp());
+	}
+
+
+	public function testAddingDays()
+	{
+		$date = new ExpressiveDate;
+		$date->addOneDay();
+		$this->assertEquals(strtotime('+1 day'), $date->getTimestamp());
+		$date->addDays(3);
+		$this->assertEquals(strtotime('+4 days'), $date->getTimestamp());
+	}
+
+
+	public function testSubtractingDays()
+	{
+		$date = new ExpressiveDate;
+		$date->minusOneDay();
+		$this->assertEquals(strtotime('-1 day'), $date->getTimestamp());
+		$date->minusDays(3);
+		$this->assertEquals(strtotime('-4 days'), $date->getTimestamp());
+	}
+
+
+	public function testAddingMonths()
+	{
+		$date = new ExpressiveDate;
+		$date->addOneMonth();
+		$this->assertEquals(strtotime('+1 month'), $date->getTimestamp());
+		$date->addMonths(3);
+		$this->assertEquals(strtotime('+4 months'), $date->getTimestamp());
+	}
+
+
+	public function testSubtractingMonths()
+	{
+		$date = new ExpressiveDate;
+		$date->minusOneMonth();
+		$this->assertEquals(strtotime('-1 month'), $date->getTimestamp());
+		$date->minusMonths(3);
+		$this->assertEquals(strtotime('-4 months'), $date->getTimestamp());
+	}
+
+
+	public function testAddingYears()
+	{
+		$date = new ExpressiveDate;
+		$date->addOneYear();
+		$this->assertEquals(strtotime('+1 year'), $date->getTimestamp());
+		$date->addYears(3);
+		$this->assertEquals(strtotime('+4 years'), $date->getTimestamp());
+	}
+
+
+	public function testSubtractingYears()
+	{
+		$date = new ExpressiveDate;
+		$date->minusOneYear();
+		$this->assertEquals(strtotime('-1 year'), $date->getTimestamp());
+		$date->minusYears(3);
+		$this->assertEquals(strtotime('-4 years'), $date->getTimestamp());
+	}
+
+
+	public function testAddingHours()
+	{
+		$date = new ExpressiveDate;
+		$date->addOneHour();
+		$this->assertEquals(strtotime('+1 hour'), $date->getTimestamp());
+		$date->addHours(3);
+		$this->assertEquals(strtotime('+4 hours'), $date->getTimestamp());
+	}
+
+
+	public function testSubtractingHours()
+	{
+		$date = new ExpressiveDate;
+		$date->minusOneHour();
+		$this->assertEquals(strtotime('-1 hour'), $date->getTimestamp());
+		$date->minusHours(3);
+		$this->assertEquals(strtotime('-4 hours'), $date->getTimestamp());
+	}
+
+
+	public function testAddingMinutes()
+	{
+		$date = new ExpressiveDate;
+		$date->addOneMinute();
+		$this->assertEquals(strtotime('+1 minute'), $date->getTimestamp());
+		$date->addMinutes(3);
+		$this->assertEquals(strtotime('+4 minutes'), $date->getTimestamp());
+	}
+
+
+	public function testSubtractingMinutes()
+	{
+		$date = new ExpressiveDate;
+		$date->minusOneMinute();
+		$this->assertEquals(strtotime('-1 minute'), $date->getTimestamp());
+		$date->minusMinutes(3);
+		$this->assertEquals(strtotime('-4 minutes'), $date->getTimestamp());
+	}
+
+
+	public function testAddingSeconds()
+	{
+		$date = new ExpressiveDate;
+		$date->addOneSecond();
+		$this->assertEquals(strtotime('+1 second'), $date->getTimestamp());
+		$date->addSeconds(3);
+		$this->assertEquals(strtotime('+4 seconds'), $date->getTimestamp());
+	}
+
+
+	public function testSubtractingSeconds()
+	{
+		$date = new ExpressiveDate;
+		$date->minusOneSecond();
+		$this->assertEquals(strtotime('-1 second'), $date->getTimestamp());
+		$date->minusSeconds(3);
+		$this->assertEquals(strtotime('-4 seconds'), $date->getTimestamp());
+	}
+
+
+	public function testAddingWeeks()
+	{
+		$date = new ExpressiveDate;
+		$date->addOneWeek();
+		$this->assertEquals(strtotime('+1 week'), $date->getTimestamp());
+		$date->addWeeks(3);
+		$this->assertEquals(strtotime('+4 weeks'), $date->getTimestamp());
+	}
+
+
+	public function testSubtractingWeeks()
+	{
+		$date = new ExpressiveDate;
+		$date->minusOneWeek();
+		$this->assertEquals(strtotime('-1 week'), $date->getTimestamp());
+		$date->minusWeeks(3);
+		$this->assertEquals(strtotime('-4 weeks'), $date->getTimestamp());
+	}
+
+
+	public function testSettingTimezoneDuringRuntime()
+	{
+		$date = new ExpressiveDate;
+		$date->setTimezone('Europe/Paris');
+		$this->assertEquals(new DateTimeZone('Europe/Paris'), $date->getTimezone());
+	}
+
+
+	public function testSetTimestampFromString()
+	{
+		$date = new ExpressiveDate;
+		$date->setTimestampFromString('Next week');
+		$this->assertEquals(strtotime('Next week'), $date->getTimestamp());
+	}
+
+
+	public function testCanCheckIfDateIsWeekday()
+	{
+		$date = new ExpressiveDate('Sunday');
+		$this->assertFalse($date->isWeekday());
+		$date->addOneDay();
+		$this->assertTrue($date->isWeekday());
+	}
+
+
+	public function testCanCheckIfDateIsWeekend()
+	{
+		$date = new ExpressiveDate('Sunday');
+		$this->assertTrue($date->isWeekend());
+		$date->addOneDay();
+		$this->assertFalse($date->isWeekend());
+	}
+
+	public function testGetDateDifferenceInYears()
+	{
+		$past = new ExpressiveDate('January 2010');
+		$future = new ExpressiveDate('January 2013');
+		$this->assertEquals(-3, $future->getDifferenceInYears($past));
+		$this->assertEquals(3, $past->getDifferenceInYears($future));
+	}
+
+
+	public function testGetDateDifferenceInMonths()
+	{
+		$past = new ExpressiveDate('January 2012');
+		$future = new ExpressiveDate('December 2012');
+		$this->assertEquals(-10, $future->getDifferenceInMonths($past));
+		$this->assertEquals(10, $past->getDifferenceInMonths($future));
+	}
+
+
+	public function testGetDateDifferenceInDays()
+	{
+		$past = new ExpressiveDate('January 12');
+		$future = new ExpressiveDate('February 15');
+		$this->assertEquals(-34, $future->getDifferenceInDays($past));
+		$this->assertEquals(34, $past->getDifferenceInDays($future));
+	}
+
+
+	public function testGetDateDifferenceInHours()
+	{
+		$past = new ExpressiveDate('-10 hours');
+		$future = new ExpressiveDate('+1 hour');
+		$this->assertEquals(-11, $future->getDifferenceInHours($past));
+		$this->assertEquals(11, $past->getDifferenceInHours($future));
+	}
+
+
+	public function testGetDateDifferenceInMinutes()
+	{
+		$past = new ExpressiveDate('-10 minutes');
+		$future = new ExpressiveDate('+1 minute');
+		$this->assertEquals(-11, $future->getDifferenceInMinutes($past));
+		$this->assertEquals(11, $past->getDifferenceInMinutes($future));
+	}
+
+
+	public function testGetDateDifferenceInSeconds()
+	{
+		$past = new ExpressiveDate('-1 day');
+		$future = new ExpressiveDate('+1 day');
+		$this->assertEquals(86400 * 2 * -1, $future->getDifferenceInSeconds($past));
+		$this->assertEquals(86400 * 2, $past->getDifferenceInSeconds($future));
+	}
+
+
+	public function testGetDateAsRelativeDate()
+	{
+		$date = new ExpressiveDate;
+		$date->minusOneDay();
+		$this->assertEquals('1 day ago', $date->getRelativeDate());
+		$date->minusDays(2);
+		$this->assertEquals('3 days ago', $date->getRelativeDate());
+		$date->addDays(4);
+		$this->assertEquals('1 day from now', $date->getRelativeDate());
+		$date->addMonths(4);
+		$this->assertEquals('4 months from now', $date->getRelativeDate());
+		$date->minusMonths(5)->minusOneYear();
+		$this->assertEquals('1 year ago', $date->getRelativeDate());
+		$date->minusYears(10);
+		$this->assertEquals('11 years ago', $date->getRelativeDate());
+	}
+
+
+	public function testGettingInvalidDateAttributeThrowsException()
+	{
+		$this->setExpectedException('InvalidArgumentException');
+		$date = new ExpressiveDate;
+		$date->getInvalidDateAttribute();
+	}
+
+
+	public function testSettingInvalidDateAttributeThrowsException()
+	{
+		$this->setExpectedException('InvalidArgumentException');
+		$date = new ExpressiveDate;
+		$date->setInvalidDateAttribute('foo');
+	}
+
+
+}
